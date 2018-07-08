bd03acfb6d13501eec5db51640412cea45fa12d7
Rikki407 <rishablamba407@gmail.com>
2018-07-08 15:36:22 +0530

    v2

--- lecture1b.js
+++ lecture1b.js
@@ -1,16 +1,29 @@
 function getSum(num1 = 1, num2 = 1) {
     console.log(`${num1}+${num2} = ${num1 + num2}`);
     console.log(
-        `${arguments[0]}+${arguments[1]} = ${arguments[0] + arguments[1]}`
+        `${arguments[0]} + ${arguments[1]} = ${arguments[0] + arguments[1]}`
     );
 }
 getSum(3);
 
 function getSumMore(...vals) {
+    console.log(...vals);
     let sum = 0;
     for (let i = 0, len = vals.length; i < len; i++) {
-        sum+= vals[i];
+        sum += vals[i];
     }
     console.log(`The sum is ${sum}`);
 }
-getSumMore(1,2,3,4);
+getSumMore(1, 2, 3, 4);
+
+let vals = [1, 2, 3, 4, 5];
+getSumMore(...vals);
+
+let difference = (num1, num2) => num1 - num2;
+console.log(difference(5, 3));
+
+const multiply = (...num) => {
+    return num[0] * num[1];
+};
+
+console.log(multiply(3, 3));
