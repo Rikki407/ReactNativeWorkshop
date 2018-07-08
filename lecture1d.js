93a6eff4aa02f5e0170581e6b29a138e388e0f70
Rikki407 <rishablamba407@gmail.com>
2018-07-08 18:43:17 +0530

    v5

--- lecture1d.js
+++ lecture1d.js
@@ -23,3 +23,43 @@ Dog.name = 'Mark';
 console.log(Dog.getInfo());
 console.log(Dog.name);
 
+let Cat = Pet.makePet('Kitty');
+console.log(Cat.name);
+
+class WorkPet extends Pet {
+    constructor(name, doWork) {
+        super(name);
+        this._doWork = doWork;
+    }
+    get doWork() {
+        return this._doWork;
+    }
+    set doWork(doWork) {
+        this._doWork = doWork;
+    }
+    getInfo() {
+        return `${this._name} is a workoholic`;
+    }
+}
+
+let Elephant = new WorkPet('Jumbo', true);
+console.log(Elephant.doWork);
+console.log(`It is ${Elephant.doWork} that ${Elephant.getInfo()}`);
+
+function getClass(classType) {
+    if(classType == 1){
+        return Pet;
+    }else{
+        return WorkPet;
+    }
+}
+
+class HomePet extends getClass(1){
+    constructor(name){
+        super(name);
+    }
+}
+
+let mouse = new HomePet('Mickey');
+console.log(mouse.getInfo());
+
