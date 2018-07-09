c29493a2ed743f4eb31b7173ecfa12657ad6a642
Rikki407 <rishablamba407@gmail.com>
2018-07-09 13:58:20 +0530

    input creation

--- day1b/App.js
+++ day1b/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Input from './src/components/input';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Input/>
       </View>
     );
   }
--- day1b/src/components/input.js
+++ day1b/src/components/input.js
@@ -1,1 +1,30 @@
-``
+import React, { Component } from "react";
+import { 
+    View,
+    Text,
+    StyleSheet
+    } from "react-native";
+
+class Input extends Component{
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Name</Text>
+                <Text>Rihab Lamba</Text>
+            </View>
+        );
+    }
+}
+export default Input;
+
+const styles = StyleSheet.create({
+    container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+    },
+    label:{
+        
+    }
+    
+});
